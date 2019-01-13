fda6a5a8ee3f1bcc9220043332bea2bd62b18336
Diwas Tamang <mtnekros@gmail.com>
2019-01-13 09:38:02 +0545

    Add project files.

---
+++ Railway Reservation System New/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+	std::cout << ".......WELCOME TO RAILWAY RESERVATION SYSTEM..........\n";
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+	do
+	{
+		std::cout << "^^^^^^^^^^^^^^^^^^^^^^MAIN MENU^^^^^^^^^^^^^^^^^^^^^^^\n\n";
+		std::cout << "Enter 1 for Admin mode\nEnter 2 for User mode\nEnter 3 for Exit\n";
+		std::cout << "Enter your choice:";
+		char response;
+		std::cin >> response;
+		std::cout << std::endl;
+		switch (response)
+
+		{
+		case 1:
+			std::cout << "Admin Stuff here\n";
+			break;
+		case 2:
+			std::cout << "User Stuff here\n";
+			break;
+		case 3:
+			return 0;
+		default:
+			std::cout << "Enter 1-3 for options\n\n";
+		}
+	} while (true);
+
+	system("pause");
+	return 0;
+}
